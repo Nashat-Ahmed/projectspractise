18b95585b73c31277d2df0ad55ec07812e657549
Nashat-Ahmed <114302755+Nashat-Ahmed@users.noreply.github.com>
2024-08-01 18:48:41 +0000

    changes in version

--- k8s-node-service/index.mjs
+++ k8s-node-service/index.mjs
@@ -5,11 +5,11 @@ const app = express()
 const PORT = 3000
 
 app.get("/",(req,res)=>{
-    const message=`hello nashaat,i am pod ${os.hostname()}`
+    const message=`hello nashaat,i am Pod ${os.hostname()} :version 1.2.1`
     res.send(message)
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, ()=>{
     console.log(`webserver is listenning at port ${PORT} `)
     console.log(os.hostname())
 })
